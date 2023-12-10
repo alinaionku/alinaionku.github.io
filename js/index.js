e46839c588514b7d8410082adab50c89787f91e7
Andzen71 <45944081+Andzen71@users.noreply.github.com>
2023-12-10 20:29:13 +0300

    fix bug with form + delete carousel.css

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
-// window.addEventListener("scroll", function(){
-//     if(this.scrollY > 50)
-//     this.document.querySelector('header').classList.add('header__bgc__on')
-//     else
-//     this.document.querySelector('header').classList.remove('header__bgc__on')
-// })
-
+////////////////////////////////// Шапка
 let header = this.document.querySelector('header');
-// let header_ul = this.document.querySelector('.header__ul')
 let scroll_prev = this.scrollY;
 let beginProzrachnost = 100;
 
-// мы можем перезагрузить страницу, где this.scrollY = 500, и получить шапку без подложки
 if(this.scrollY > beginProzrachnost){
     prozrachnost = (this.scrollY-beginProzrachnost)/100;
     header.style.background = 
@@ -18,9 +10,7 @@ if(this.scrollY > beginProzrachnost){
     "), hsla(223, 87.8%, 32.2%,"+prozrachnost+
     "), hsla(223, 84.7%, 23.1%,"+prozrachnost+
     "), hsla(223, 84.7%, 23.1%,"+prozrachnost;
-    // console.log(header.style.background)
 }
-// Этот листенер отвечает за шапку сайта и его пропажу\появление
 window.addEventListener("scroll", function(){
     if(this.scrollY > beginProzrachnost && this.scrollY > scroll_prev){
         prozrachnost = (this.scrollY-beginProzrachnost)/100;
@@ -29,7 +19,6 @@ window.addEventListener("scroll", function(){
         "), hsla(223, 87.8%, 32.2%,"+prozrachnost+
         "), hsla(223, 84.7%, 23.1%,"+prozrachnost+
         "), hsla(223, 84.7%, 23.1%,"+prozrachnost;
-        // console.log(header.style.background)
     }
     else if(this.scrollY < scroll_prev ){
         prozrachnost = (this.scrollY-beginProzrachnost)/100;
@@ -41,15 +30,12 @@ window.addEventListener("scroll", function(){
         "), hsla(223, 87.8%, 32.2%,"+prozrachnost+
         "), hsla(223, 84.7%, 23.1%,"+prozrachnost+
         "), hsla(223, 84.7%, 23.1%,"+prozrachnost;
-    }
-    // console.log(scroll_prev, this.scrollY, "before")
+    } 
     scroll_prev = this.scrollY;
-    // console.log(scroll_prev, this.scrollY, "after")
-
-    // && this.scrollY < beginProzrachnost
 })
+////////////////////////////////// Шапка
 
-// Этот листенер отвечает за затемнение при достижении блока "Портфолио"
+////////////////////////////////// Затемнение "Портфолио"
 let portfolio = document.querySelector(".portfolio");
 let portfolio_begin = portfolio.offsetTop;
 let portfolio_end = portfolio.offsetTop + portfolio.clientHeight;
@@ -61,21 +47,17 @@ let end_whitness = portfolio_end + window.innerHeight/3;
 
 
 window.addEventListener("scroll", function(){
-    // if ((this.scrollY) > start_darkness && (this.scrollY < end_darkness)){
     if ((this.scrollY) > start_darkness && (this.scrollY < start_whitness)){
-        // this.document.body.classList.remove("body-main");
         this.document.body.classList.add("body-dark");
-        // this.document.body.style.backgroundColor = "rgba(0,9,66," + (this.scrollY - start_darkness)/(end_darkness - start_darkness)*0.6+")"; 
     };
-    // if ((this.scrollY > start_whitness) && (this.scrollY < end_whitness)){
     if ((this.scrollY > start_whitness) || (this.scrollY < start_darkness)){
         this.document.body.classList.remove("body-dark");
-        // this.document.body.classList.add("body-main");
-        // this.document.body.style.backgroundColor = "rgba(0,9,66," + (end_whitness - this.scrollY)/(end_whitness - start_whitness)*0.6+")"; 
     };
 
 })
+////////////////////////////////// Затемнение "Портфолио"
 
+////////////////////////////////// Форма
 // Это отвечает за увеличение размера textarea (где написано "Ваше сообщение")
 document.getElementById('textArea').style.height = '2rem';
 document.addEventListener('DOMContentLoaded', function() {
@@ -103,16 +85,16 @@ document.addEventListener('DOMContentLoaded', function() {
 //селектор выбора
 
 function toggleOptions() {
+    
     const optionsContainer = document.querySelector('.options-container');
     optionsContainer.style.display = optionsContainer.style.display === 'none' ? 'block' : 'none';
-  }
+}
 
-  function selectOption(value) {
+function selectOption(value) {
     const visibleText = document.querySelector('.visible-text');
     visibleText.textContent = ` ${value}`;
     visibleText.style.opacity = 1;
-    toggleOptions();
-  }
+}
 
-//   тыкнуть не туда, чтобы селектор выбора закрылся
+////////////////////////////////// Форма
 
